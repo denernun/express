fd1d36dc50c2ff5fdce85310ac656b0a23f9dbab
Dener Rocha <denernun@gmail.com>
2021-02-22 23:24:12 -0300

    .

--- bin/server-certs.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const path = require('path');
-const fs = require('fs');
-
-exports.certificate = fs.readFileSync(path.join(__dirname, './../certs/localhost.crt')).toString();
-exports.privateKey = fs.readFileSync(path.join(__dirname, './../certs/localhost.key')).toString();
--- socket/socket-routes.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-exports.routes = (socket) => {
-  socket.on('click', click => {
-    socket.emit('click', { 'server send click accept': click });
-    socket.broadcast.emit('click', { 'server send click broadcast': click });
-  });
-};
