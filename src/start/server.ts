0a7b70c13c45f8297434d79657bc26c71397557a
Dener Rocha <denernun@gmail.com>
2021-03-06 19:34:23 -0300

    .

--- src/start/server.ts
+++
@@ -1,7 +0,0 @@
-import app from './app';
-
-const port = parseInt(process.env.APP_PORT as string) || 3000;
-
-app.listen(port, () => {
-  console.log('\x1b[33m%s\x1b[0m', `=> 🚀 Server running on the port: ${port}`);
-});
